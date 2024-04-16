72db9e9eee31e62769933c19089f2056ae6a77c3
TalhaDogar007 <talhadogar003@gmail.com>
2024-04-16 16:42:40 +0100

    remove code

--- src/Hooks/useRef/useRef.tsx
+++ src/Hooks/useRef/useRef.tsx
@@ -1,22 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 
-const MyComponent: React.FC = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    // Focus on the input element when the component mounts
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, []); // Empty dependency array ensures the effect runs only once on mount
-
-  return (
-    <div>
-      <h1>My Component</h1>
-      <input ref={inputRef} type="text" />
-    </div>
-  );
-};
 
 const UseRef: React.FC = () => {
     return <MyComponent />;
